b486d8146aace13bd01930f0054f0a5a1be60f1a
Dave Ada <davealyinovich@gmail.com>
2018-11-29 00:14:00 -0600

    header, jumbo, about us section done, for now.  adding instascroll and footer to finish first draft of homepage.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,41 @@
-$(document).ready(function () {
-  $('.aboutusphoto').slick({
-   arrows: false,
-   autoplay: true,
-   autoplaySpeed: 3000,
-   fade: true,
-   infinite: true,
- });
-});
-
-
 $(document).ready(function(){
-var userFeed = new Instafeed({
-	get: 'user',
-	userId: '5826265013',
-	limit: 4,
-	resolution: 'standard_resolution',
-	accessToken: '5826265013.1677ed0.8c78148832734754920436acffd6dfb0',
-	sortBy: 'most-recent',
-	template: '<div class="gallery"><a href="https://www.instagram.com/tacotlantacos/?hl=en" title="Click to visit our Instagram page" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
-})
-userFeed.run();
+$('.familycarousel').slick({
+    speed: 8000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: true,
+    initialSlide: 1,
+    arrows: false,
+    buttons: false
+});
 });
 
 $(document).ready(function(){
-	$(".deliverytab").click(function(){
-		$(".vendormain").toggle();
-	});
-})
+$('.ourstoryimages').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+});
+
+$(document).ready(function(){
+    $(".demo").myig(
+        ins_id = 503807090, // your user_id
+        ins_count = 10, // Count of media to return
+        ins_token = '141970.467ede5.edbc9c37472d41b790e1db8948793f11' // your token
+    );
+});
+
+$(document).ready(function(){    
+    $(".myig_popup").fancybox({
+        openEffect : 'fade',
+        closeEffect : 'fade'
+    });
+});
